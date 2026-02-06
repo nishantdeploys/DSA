cba4d44ddddaf51c7a6b3598a2b70ab1d01f9e27
Nishant Kumar <nishant098097@gmail.com>
2026-02-06 23:02:32 +0530

    Codeforeces

---
+++ wrongSubtraction.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    int k;
+    cin >> n >> k;
+
+    while (k--) {
+        if (n % 10 != 0)
+            n = n - 1;
+        else
+            n = n / 10;
+    }
+
+    cout << n << endl;
+    return 0;
+}
