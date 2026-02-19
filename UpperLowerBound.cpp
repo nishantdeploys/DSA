7be938a39565e72c3ec4abee1096e156b85a4833
Nishant <nishant098097@gmail.com>
2026-02-19 21:52:02 +0530

    CodeForces

--- UpperLowerBound.cpp
+++ UpperLowerBound.cpp
@@ -5,7 +5,7 @@ int main(){
     // lower bound
 
     vector<int> v = {10,20,30,40,50};
-
+u
     auto it = lower_bound(v.begin(),v.end(),30);
 
     cout << *it << endl;
@@ -16,7 +16,8 @@ int main(){
 
     vector<int> vec = {10,20,30,40,50};
 
-    auto itr = upper_bound(vec.begin(),vec.end(),30);
+    auto itr = upper_bound(vec.begin(),v.end(),30);
 
     cout << *itr << endl;
 }
+
