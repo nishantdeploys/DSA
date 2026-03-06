e0448f30f6b71d1ffd13b75d825489c7b8a39db0
Nishant <nishant098097@gmail.com>
2026-03-06 15:48:59 +0530

    CodeForces

---
+++ maximumGcd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int n){
+    cout << n/2 << endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int k;
+        cin >> k;
+        solve(k);
+    }
+    return 0;
+}
