598756d3b43f4ddc98f1c159850b70f1fc6bc012
Nishant <nishant098097@gmail.com>
2026-03-27 19:28:09 +0530

    CodeForces

---
+++ AntonAndLetter.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    string s;
+    getline(cin , s);
+    set<char> st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='a' && s[i]<='z'){
+            st.insert(s[i]);
+        }
+    }
+    cout << st.size() << endl;
+}
