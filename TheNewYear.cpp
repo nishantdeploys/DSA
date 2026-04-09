63931e021842daa60ad32447ba369d29931c3422
Nishant <nishant098097@gmail.com>
2026-04-09 22:51:54 +0530

    CodeForces

---
+++ TheNewYear.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+
+    vector<int> v = {x1, x2, x3};
+    sort(v.begin(), v.end());
+
+    int ans = v[2] - v[0]; // max - min
+
+    cout << ans << endl;
+
+    return 0;
+}
