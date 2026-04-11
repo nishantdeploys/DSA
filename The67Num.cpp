7a931f781da5dac848f1ac6446b31e986155be3b
Nishant <nishant098097@gmail.com>
2026-04-11 23:39:09 +0530

    CodeForces

---
+++ The67Num.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    while(n--){
+        int x;
+        cin >> x;
+        cout << x << endl;
+    }
+    return 0;
+
+}
