d85ff599c2db62e87c5c7b6c985eb9f53c24db41
Nishant Kumar <nishant098097@gmail.com>
2026-04-23 21:10:08 +0530

    CodeForces

---
+++ OddOneOut.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        if (a == b) {
+            cout << c << '\n';
+        } else if (a == c) {
+            cout << b << '\n';
+        } else {
+            cout << a << '\n';
+        }
+    }
+    
+    return 0;
+}
